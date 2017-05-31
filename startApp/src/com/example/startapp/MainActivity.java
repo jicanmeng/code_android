2b8acf461696612c824ffa6cb94624924c838c63
jicanmeng <jicanmeng@126.com>
2017-05-31 14:38:28 +0800

    2. add another sample: start other app through getLaunchIntentForPackage() and startActivity()

---
+++ startApp/src/com/example/startapp/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.startapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        PackageManager packageManager = getPackageManager();
+        Intent intent = new Intent();
+        intent = packageManager.getLaunchIntentForPackage("com.example.accsensordata");
+        if(intent == null) {
+            Log.e("demo", "app not found");
+        } else {
+            startActivity(intent);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+	}
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
